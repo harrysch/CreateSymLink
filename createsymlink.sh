d273a6f51b7a0fb794a2c0fdec02b4334aef73e8
harald <harald@iMemphis.fritz.box>
2019-01-03 15:58:16 +0100

    changes in script and workflow

--- createsymlink.sh
+++ createsymlink.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 # version with clean input
 
+echo "DEBUG:" > $HOME/debug_createsymlink.txt
 
 SH_DEBUG_ON="ON"
 SH_DEBUG_OFF="OFF"
-# 
-SH_DEBUG=$SH_DEBUG_OFF
+
+SH_DEBUG=$SH_DEBUG_ON
+#SH_DEBUG=$SH_DEBUG_OFF
 
 function d_echo {
 	if [ $SH_DEBUG == $SH_DEBUG_ON ]
 	then
-		echo "DEBUG:"$* 
+		echo "DEBUG::"$*"::" 
+		echo "DEBUG::"$*"::" >> $HOME/debug_createsymlink.txt
 	fi
 
 }  
@@ -44,16 +47,16 @@ do
 		then
 			a_dirname="$name"				
 		else
-			a_filename=basename "$name"
-			a_dirname=dirname "$name"
-			echo "filename:"$a_filename
-			echo "dir:"$a_dirname
+			#a_filename=basename "$name"
+			#a_dirname=dirname "$name"
+			d_echo "filename:"$a_filename
+			d_echo "dir:"$a_dirname
 		fi
 			
 	newname=$name$fileSuffix
 	
 	# if writing is allowed
-	if [ -w $a_dirname ]
+	if [ -w "$a_dirname" ]
 		then 
 			if [ ! -e "$newname" ]
     		then
@@ -62,7 +65,7 @@ do
     		fi
 	else
 		# if writing is NOT allowed ... write to: $HOME/Desktop	
-		d_echo "writing in old path is NOT allowed"
+		d_echo "writing in old path is NOT allowed:"$a_dirname
 		newname=$HOME/Desktop/$a_filename$fileSuffix
 		if [ ! -e "$newname" ]
     	then 	
